097fb828bb0942868afe50296f696e95dc5c2a49
Anthony Ramirez <ar4477@nyu.edu>
2019-04-23 17:08:16 -0400

    add more views

---
+++ PortableClasses/PortableClasses/CourseViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CourseViewController.swift
+//  PortableClasses
+//
+//  Created by Anthony Ramirez on 4/23/19.
+//  Copyright © 2019 nyu.edu. All rights reserved.
+//
+
+import Foundation
---
+++ PortableClasses/PortableClasses/RegisterViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterViewController.swift
+//  PortableClasses
+//
+//  Created by Anthony Ramirez on 4/23/19.
+//  Copyright © 2019 nyu.edu. All rights reserved.
+//
+
+import Foundation
