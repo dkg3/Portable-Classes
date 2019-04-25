4f81538ac04a082de7a6623e6f05ff2a70f22b24
david krauskopf-greene <davidkrauskopf-greene@davids-MacBook-Pro-24.local>
2019-04-24 23:00:15 -0400

    map tracks location

---
+++ PortableClasses/PortableClasses/MapViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MapViewController.swift
+//  PortableClasses
+//
+//  Created by david krauskopf-greene on 4/24/19.
+//  Copyright © 2019 nyu.edu. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+import Firebase
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        mapView.showsUserLocation = true
+        if CLLocationManager.locationServicesEnabled() == true {
+            if CLLocationManager.authorizationStatus() == .restricted || CLLocationManager.authorizationStatus() == .denied || CLLocationManager.authorizationStatus() == .notDetermined {
+                locationManager.requestWhenInUseAuthorization()
+            }
+            locationManager.desiredAccuracy = 1.0
+            locationManager.delegate = self
+            locationManager.startUpdatingLocation()
+        }
+        else {
+            print("Please turn on location services or GPS")
+        }
+    }
+    
+    //MARK: CLLocationManager Delagates
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.002, longitudeDelta: 0.002))
+        self.mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Unable to access your current location")
+    }
+    
+}
