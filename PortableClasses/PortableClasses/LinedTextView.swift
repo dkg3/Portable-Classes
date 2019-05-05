119fa4cdc798e7ca6d8d815b772f50dda0b079b4
david krauskopf-greene <davidkrauskopf-greene@davids-MacBook-Pro-24.local>
2019-05-05 14:46:50 -0400

    lines to notes

---
+++ PortableClasses/PortableClasses/LinedTextView.swift
@@ -0,0 +1,21 @@
+//
+//  LinedTextView.swift
+//  PortableClasses
+//
+//  Created by david krauskopf-greene on 5/5/19.
+//  Copyright © 2019 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class LinedTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
